7f6a635f0167b75917893e0819f344d83dc4926e
IlyaVysokikh <vysokikh.ilya03@gmail.com>
2023-04-30 09:46:06 +0500

    Добавлены абстактные классы для оценок

--- headers/Evaluations.h
+++ headers/Evaluations.h
@@ -6,7 +6,80 @@ class Evaluation
 {
 public:
     virtual float calculate() = 0;
-private:
+protected:
     std::vector<float> sample;
+};
 
+class Mean: public Evaluation
+{
+public:
+    Mean(std::string path);
+    Mean(std::vector<float> s);
+    float calculate() override;
+};
+
+class SampleVariance: public Evaluation
+{
+public:
+    SampleVariance(std::string path);
+    SampleVariance(std::vector<float> s);
+    float calculate() override;
+};
+
+class GiniDifference: public Evaluation
+{
+public:
+    GiniDifference(std::string path);
+    GiniDifference(std::vector<float> s);
+    float calculate() override;
+};
+
+class CentralMoment: public Evaluation
+{
+private:
+    int k;
+public:
+    CentralMoment(std::string path);
+    CentralMoment(std::vector<float> s);
+    float calculate() override;
+};
+
+class Quantile: public Evaluation
+{
+public:
+    Quantile(std::string path);
+    Quantile(std::vector<float> s);
+    float calculate() override;
+};
+
+class Quantile: public Evaluation
+{
+public:
+    Quantile(std::string path);
+    Quantile(std::vector<float> s);
+    float calculate() override;
+};
+
+class AssymetryFactor: public Evaluation
+{
+public:
+    AssymetryFactor(std::string path);
+    AssymetryFactor(std::vector<float> s);
+    float calculate() override;
+};
+
+class KurtorisFactor: public Evaluation
+{
+public:
+    KurtorisFactor(std::string path);
+    KurtorisFactor(std::vector<float> s);
+    float calculate() override;
+};
+
+class HodgesLehmann: public Evaluation
+{
+public:
+    HodgesLehmann(std::string path);
+    HodgesLehmann(std::vector<float> s);
+    float calculate() override;
 };
